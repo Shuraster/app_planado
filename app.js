5673c20023b034ef40ad6173fcd993984e1782ac
Aleksandra <aleksandra.chernikova@orion.team>
2018-09-01 17:01:18 +0300

    Добавлены недостающие файлы

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var app = express();
 
-
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 
 app.set('view engine', 'html');
 
 app.get('/admin', function(req, res) {
-    res.sendfile('views/pages/admin.html')
+    res.sendfile('./admin.html')
+  });
+  
+app.get('/login', function(req, res) {
+    res.sendfile('./login.html')
   });
 
 app.get('/counter', function(req, res){
-    res.sendfile('views/pages/counter.html')
+    res.sendfile('./counter.html')
 });
 
-
+//app.get('/base.js', function(req, res){
+//    res.sendfile('models/base.js')
+//});
 
 app.listen(8080, function() {
     console.log('Listen port 8080')
---
+++ js/base.js
@@ -0,0 +1,17 @@
+var login = 'aswwasdda';
+var name = 'asasasddd';
+var surname = 'aaasdsdsdas';
+var address = 'asdaasdasdsdasdaasd';
+//import {addNewUser} from '/app';
+/*module.exports = function addUser(login, name, surname, address) {*/
+var client;
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://master:pass@localhost:5432/database';
+client = new pg.Client(connectionString);
+client.connect();
+   
+var InsertQuery = 'INSERT INTO users (login, userRole, name, surname, userAdr ) VALUES ($1, $2, $3, $4, $5)';
+const query = client.query(InsertQuery,[login, 1, name, surname, address]);
+query.on('end', () => { client.end(); });
+//}
+//export {addUser};
